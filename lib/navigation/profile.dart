34dc06379570b2ca5e0edc7a3d731bfaccb167c4
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2025-01-10 20:01:07 +0200

    Update profile.dart

--- lib/navigation/profile.dart
+++ lib/navigation/profile.dart
@@ -4,35 +4,29 @@ import 'package:tourism_app/signup.dart';
 class ProfilePage extends StatelessWidget {
   String _hashPassword(String password) {
     return '*' *
-        password.length; //function to display password hashed for security
+        password
+            .length; //function to display asterisks instead of the actual password for security
   }
 
   @override
   Widget build(BuildContext context) {
-    final storedFirstName = SignUpPage.userCredentials['fullName'] ?? 'N/A';
+    final storedFirstName = SignUpPage.userCredentials['firstName'] ?? 'N/A';
     final storedLastName = SignUpPage.userCredentials['lastName'] ?? 'N/A';
     final storedEmail = SignUpPage.userCredentials['email'] ?? 'N/A';
     final storedPassword = SignUpPage.userCredentials['password'] ?? 'N/A';
-    final storedPhoneNumber = SignUpPage.userCredentials['phoneNumber'] ?? 'N/A';
+    final storedPhoneNumber =
+        SignUpPage.userCredentials['phoneNumber'] ?? 'N/A';
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Profile',
-            style: TextStyle(
-              fontSize: 26,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Cinzel',
-            )),
-        backgroundColor: Color(0xFFf5ebe0),
-      ),
       body: Center(
         child: Container(
           height: screenHeight * 0.7,
           width: screenWidth * 0.9,
           decoration: BoxDecoration(
+            boxShadow: kElevationToShadow[4],
             color: Color(0xFFf5ebe0),
             borderRadius: BorderRadius.circular(20),
           ),
@@ -121,7 +115,7 @@ class ProfilePage extends StatelessWidget {
                   color: const Color(0xFFbc6c25),
                   onPressed: () {
                     Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => SignUpPage()));      //to return to sign up page
+                        MaterialPageRoute(builder: (context) => SignUpPage()));
                   },
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
